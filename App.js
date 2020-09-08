c85efd60fa8feffd9db08aa00f76dc37c4fb1757
abhijay24 <abhijay24.3.7@gmail.com>
2020-09-08 21:11:32 +0530

    version mismatched

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import BookTransaction from './screens/BookTransaction'
+import SearchScreen from './screens/SearchScreen'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+ render(){
+  return(
+    <AppContainer/>
+  )
+
+ }
+
 }
+const TabNavigator = createBottomTabNavigator({
+  Transaction : {screen:BookTransaction},
+  Search : {screen:SearchScreen}
+})
+const AppContainer = createAppContainer(TabNavigator)
 
 const styles = StyleSheet.create({
   container: {
